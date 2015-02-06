530800e084b00a73448065b483b13f5ac24dec03
cmoraski <cmoraski23@gmail.com>
2015-02-06 13:24:19 -0500

    Update ALDG.java

--- ALDG.java
+++ ALDG.java
@@ -1,5 +1,12 @@
+package asgn01;
+
+import java.util.Scanner;
+
 //Adjacency List Directed Graph
 public class ALDG extends ALGraph{
 
-	
+	public ALDG(Scanner sc) {
+		super(sc, false, true);
+	}
+
 }
