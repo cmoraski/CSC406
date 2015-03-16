ec36177036dd808d5ff2949a5aacba73811fb285
cmoraski <cmoraski23@gmail.com>
2015-03-16 15:35:50 -0400

    Create Warshall.java

---
+++ Warshall.java
@@ -0,0 +1,32 @@
+package asgn03;
+
+public class Warshall {
+
+	int [][] r;
+
+	public Warshall(int[][]a){
+		r = new int[a.length][a.length];
+		for (int i = 0; i < a.length; i++){
+			for (int j = 0; j < a[0].length; j++)
+				r[i][j] = a[i][j];
+		}
+		doWarshalls();	
+	}
+	
+	private void doWarshalls(){
+		for (int k = 0; k < r.length; k++){
+			for (int i = 0; i < r.length; i++){
+				for (int j = 0; j < r.length; j++){
+					if (r[i][k] == 1 && r[k][j] == 1)
+						r[i][j] = 1;
+				}
+			}
+		}
+	}
+	
+	public int[][] getWarshalls(){
+		return r;
+	}
+	
+
+}
